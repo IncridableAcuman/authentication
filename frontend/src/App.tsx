76572177f596ad88d069f5dd81d6c9f2fc33eddc
Izzatbek <abdusharipovizzat03@gmail.com>
2025-11-12 10:48:57 +0500

    client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { Button } from "./components/ui/button"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Auth from "./pages/Auth"
 
 const App = () => {
   return (
-    <div>
-      <Button variant={'outline'} >Click here</Button>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/auth" element={<Auth/>} />
+    </Routes>
+    </>
   )
 }
 
